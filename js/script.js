f070984367e84a8acb35fc145c8bc882de8765a6
luminan-dev <pardaevasadbek02@gmail.com>
2025-07-07 14:28:39 +0500

    changes

--- js/script.js
+++ js/script.js
@@ -25,11 +25,41 @@ var swiper = new Swiper(".bottom-swiper", {
         el: ".swiper-scrollbar",
       },
 });
-var swiper = new Swiper(".end-mini-swipers", {
+// var swiper = new Swiper(".end-mini-swipers", {
+//       slidesPerView: 3,
+//       spaceBetween: 20,
+//       pagination: {
+//         el: ".swiper-pagination-review",
+//         clickable: true,
+//       },
+// });
+var testimonialSwiper = new Swiper(".end-mini-swipers", {
+  // Default slides configuration
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination-review",
+    clickable: true,
+    bulletClass: 'swiper-pagination-bullet',
+    bulletActiveClass: 'swiper-pagination-bullet-active',
+    renderBullet: function (index, className) {
+      return `<span class="${className}"></span>`;
+    },
+  },
+  
+  // Responsive breakpoints
+  breakpoints: {
+    // When window width is >= 576px
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    // When window width is >= 992px
+    992: {
       slidesPerView: 3,
-      spaceBetween: 20,
-      pagination: {
-        el: ".swiper-pagination-review",
-        clickable: true,
-      },
+      spaceBetween: 40
+    }
+  }
 });
+
+  
